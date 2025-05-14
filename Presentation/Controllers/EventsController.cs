52bba555890d2a1c01d6c4db093ddfef8f99e8a6
Gerda <gerdabrodin@gmail.com>
2025-05-14 15:38:12 +0200

    Init

---
+++ Presentation/Controllers/EventsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Services;
+
+namespace Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class EventsController(EventService eventService) : ControllerBase
+{
+    private readonly EventService _eventService = eventService;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _eventService.GetAllAsync();
+        return Ok(result);
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetAll(string Id)
+    {
+        var result = await _eventService.GetAsync(Id);
+
+        return result != null ? Ok(result) : NotFound();
+
+    }
+}
---
+++ Presentation/Data/EventEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.Data;
+
+public class EventEntity
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    public string Name { get; set; } = null!;
+
+}
